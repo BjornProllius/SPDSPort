7854cdd81f09f84ff8efaa406220da7f7cdfa15c
verdantliberatus <122659133+verdantliberatus@users.noreply.github.com>
2023-12-06 15:06:51 -0700

    upoprule and upushrule compilation errors fixed

--- WPDS/src/main/scala/wpds/impl/UPopRule.scala
+++ WPDS/src/main/scala/wpds/impl/UPopRule.scala
@@ -2,7 +2,7 @@ package wpds.impl
 
 import wpds.interfaces.{Location, State}
 
-class UPopRule[N <: Location, D <: State] extends PopRule[N, D, Weight.NoWeight](s1: D, l1: N, s2: D, Weight.NO_WEIGHT_ONE) {
+class UPopRule[N <: Location, D <: State](s1: D, l1: N, s2: D) extends PopRule[N, D, Weight.NoWeight](s1, l1, s2, Weight.NO_WEIGHT_ONE) {
 
     override def toString: String = {
         "<" + s1 + ";" + l1 + ">-><" + s2 + ">"
--- WPDS/src/main/scala/wpds/impl/UPushRule.scala
+++ WPDS/src/main/scala/wpds/impl/UPushRule.scala
@@ -3,7 +3,7 @@ package wpds.impl
 import wpds.interfaces.{Location, State}
 import wpds.impl.Weight
 
-class UPushRule[N <: Location, D <: State] extends PushRule[N, D, Weight.NoWeight](s1: D, l1: N, s2: D, l2: N, callSite: N, Weight.NO_WEIGHT_ONE) {
+class UPushRule[N <: Location, D <: State](s1: D, l1: N, s2: D, l2: N, callSite: N) extends PushRule[N, D, Weight.NoWeight](s1, l1, s2, l2, callSite, Weight.NO_WEIGHT_ONE) {
 
     override def toString: String = {
         "<" + s1 + ";" + l1 + ">-><" + s2 + ";" + l2 + "." + callSite + ">"
